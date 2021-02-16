3a527230c02ebca77c3f59bd0955443de81fdc4a
zhenghui wang <zhenghui.wang.k@gmail.com>
2021-02-16 00:55:02 +0000

    Update scout.ex

--- lib/scout.ex
+++ lib/scout.ex
@@ -1,25 +1,36 @@
 defmodule Scout do
 
 def start config, leader, acceptors, ballot do
-	send acceptors, {:p1a, self(), ballot}
-	next config, acceptors, 0, ballot, leader, acceptors
+	for acceptor <- acceptors do
+		send acceptor, {:p1a, self(), ballot}
+	end
+	send config.monitor, { :SCOUT_SPAWNED, config.node_num }
+	next config, MapSet.new(acceptors), MapSet.new, ballot, leader, acceptors
 
 end
 
-defp next config waitfor, pvalues, ballot, leader, acceptors do
+defp next config, waitfor, pvalues, ballot, leader, acceptors do
 	receive do
 		{:p1b, acceptors, ballot_,r} ->
+			IO.puts "receive p1b"
 			if ballot_ == ballot do
-				pvalues = pvalues ++ r
+				pvalues = MapSet.union(pvalues,r)
 				waitfor = MapSet.delete(waitfor,r)
-				if MapSet.size(waitfor) < length(acceptors)/2 do
+				#IO.inspect (IEx.Info.info(acceptors))
+				# IT SHOULD BE .... COMMENT FOR DEBUG
+				if 2 * MapSet.size(waitfor) < length(acceptors) do
+				#if !(2 * MapSet.size(waitfor)) do
 					send leader, {:adopted, ballot, pvalues}
 					exit(:normal)
-				end
+				else
+			        next config, waitfor, pvalues, ballot, leader, acceptors
+		      	end
 			else
 				send leader, {:preempted, ballot_}
+            	send config.monitor, { :SCOUT_FINISHED, config.node_num }
 				exit(:normal)
 			end
+		_ -> IO.puts "!Scout-next function received unexpected msg"
 	end
 end
 
